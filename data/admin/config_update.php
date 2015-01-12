6f62186967ad605a46f10761483c06f93299b1a5
unknown <wuhaiyan-s@wuhaiyan-D1.corp.qihoo.net>
2015-01-12 14:31:23 +0800

    modified:   data/admin/allowurl.txt 	modified:   data/admin/config_update.php 	modified:   data/admin/source.txt 	modified:   templets/default/head.htm 	modified:   templets/default/head2.htm 	modified:   templets/default/images/logo.gif 	modified:   templets/default/index.htm 	modified:   templets/default/search.htm

--- data/admin/config_update.php
+++ data/admin/config_update.php
@@ -10,5 +10,5 @@
  */
 
 //更新服务器，如果有变动，请到 http://bbs.dedecms.com 查询
-$updateHost = 'http://updatenew.dedecms.com/base-v57/';
-$linkHost = 'http://flink.dedecms.com/server_url.php';
+$updateHost = '';
+$linkHost = '';
