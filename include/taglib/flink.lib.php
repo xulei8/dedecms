28c598125fb73e8e717c410c31180452658804f4
xulei <xulei8@qq.com>
2015-11-17 15:14:20 +0800

    oktag

--- include/taglib/flink.lib.php
+++ include/taglib/flink.lib.php
@@ -77,13 +77,7 @@ function lib_flink(&$ctag,&$refObj)
 				}
 			} else {
 				$revalue=<<<EOT
-<li><a href='http://ad.dedecms.com' target='_blank' title='DedeCMS广告'>DedeCMS广告</a></li><li><a href='http://service.dedecms.com' target='_blank' title='织梦客户服务中心'>
-织梦客户服务中心</a></li><li><a href='http://ask.dedecms.com' target='_blank' title='织梦问答'>
-织梦问答</a></li><li><a href='http://tools.dedecms.com' target='_blank' title='站长工具'>
-站长工具</a></li><li><a href='http://site.desdev.cn' target='_blank' title='DedeCMS建站中心'>
-DedeCMS建站中心</a></li><li><a href='http://help.dedecms.com' target='_blank' title='织梦CMS帮助中心'>
-织梦CMS帮助中心</a></li><li><a href='http://' target='_blank' title=''>
-</a></li>
+ 
 EOT;
 			}
 			$row['reval'] = $revalue;
