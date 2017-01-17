b72e99445ad7ea3a5c211bec65e4cf39904f2c34
xuleinewvm <xulei8@qq.com>
2017-01-17 19:34:23 +0800

    phpexcel_export_and_import

--- include/extend.func.php
+++ include/extend.func.php
@@ -60,3 +60,75 @@ function sendemail($emails = array(),$title = '' ,$body = '',$attachments = arra
 	    return true;
 	}
 }
+
+/*
+ * export data to .xls
+ * @params $data = array(
+	0=>array('product name','price','sale num'),
+	1=>array('Cupshe 1','$21.99',30),
+	2=>array('Cupshe 2','$19.99',20),
+	);
+ */
+function export_xls($data)
+{
+	/** Include PHPExcel */
+	require_once dirname(__FILE__) . '/PHPExcel/Classes/PHPExcel.php';
+	// Create new PHPExcel object
+	$objPHPExcel = new PHPExcel();
+
+	// Set document properties
+	$objPHPExcel->getProperties()->setCreator("Maarten Balliauw")
+								 ->setLastModifiedBy("Maarten Balliauw")
+								 ->setTitle("Office 2007 XLSX Test Document")
+								 ->setSubject("Office 2007 XLSX Test Document")
+								 ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
+								 ->setKeywords("office 2007 openxml php")
+								 ->setCategory("Test result file");
+
+    // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+	$objPHPExcel->setActiveSheetIndex(0);
+
+	// Add some data
+	$objPHPExcel->setActiveSheetIndex(0)->fromArray(
+                $data, // 赋值的数组
+                NULL, // 忽略的值,不会在excel中显示
+                'A1');
+
+	// Rename worksheet
+	//$objPHPExcel->getActiveSheet()->setTitle('Simple');
+
+	// Redirect output to a client’s web browser (Excel5)
+	header('Content-Type: application/vnd.ms-excel');
+	header('Content-Disposition: attachment;filename="01simple.xls"');
+	header('Cache-Control: max-age=0');
+	// If you're serving to IE 9, then the following may be needed
+	header('Cache-Control: max-age=1');
+
+	// If you're serving to IE over SSL, then the following may be needed
+	header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+	header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+	header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+	header ('Pragma: public'); // HTTP/1.0
+
+	$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+	$objWriter->save('php://output');
+	exit;
+}
+
+/*
+ * read data from files type of .xls
+ * @params $file string eg:./products.xls
+ */
+function read_xls($file)
+{
+	/** Include PHPExcel_IOFactory */
+	require_once dirname(__FILE__) . '/PHPExcel/Classes/PHPExcel/IOFactory.php';
+
+	if (!file_exists($file)) {
+		exit("File {$file} is not exist.\n");
+	}
+
+	$objPHPExcel = PHPExcel_IOFactory::load($file);
+	$excelData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
+    return $excelData;
+}
